ba79cb01ac7ec15fef348a6d4860b944d9f50ce4
e-poeta <eleonora.poeta@polito.it>
2026-02-03 11:55:51 +0100

    Update train and trainer

--- training/train.py
+++ training/train.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import warnings
 from dataloader.load_monai_metadata import load_train_val_metadata
 from configs.transforms_config import get_transforms
-
+from fairness.loss_divergence import DivergenceFairLoss
 from monai.data import Dataset, DataLoader, CacheDataset,PersistentDataset
 
 from trainer import Trainer
@@ -33,15 +33,22 @@ parser.add_argument("--epochs", type=int, default=500,
                     help="Number of training epochs")
 parser.add_argument("--val_interval", type=int,
                     default=1, help="Validation interval")
-parser.add_argument("--monai", action="store_true", help="Use MONAI pipeline")
+parser.add_argument("--monai", action="store_true", help="Use MONAI pipeline", default=True)
+parser.add_argument("--divloss", action="store_true", help="Use divergence loss", default=True)
 parser.add_argument("--resume", type=str, default=None, help="Name of the checkpoint")
+parser.add_argument("--alpha", type=float, default=0.5, help="Weight for the divergence loss")
+parser.add_argument("--columns_of_interest", type=str, nargs="+", default=["age", "breast_density", "menopause"], help="Columns of interest for divergence loss")
+parser.add_argument("--load_checkpoint", action="store_true", help="Load model checkpoint", default=False)
+parser.add_argument("--min_support", type=float, default=0.005, help="Minimum support for subgroup discovery")
+
 
 
 args = parser.parse_args()
 os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
 
 
-USE_MONAI_PIPELINE = args.monai 
+USE_MONAI_PIPELINE = args.monai  
+USE_DIVLOSS = args.divloss  
 
 # CONFIG
 set_determinism(42)
@@ -49,23 +56,27 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 config = {
     "device": device,
     "epochs": args.epochs,
+    "starting_epoch": 0,
     "learning_rate": 1e-4,
     "val_interval": args.val_interval,
     "project_name": "mamma-mia",
     "run_name": args.exp_name,
     "model_save_path": "models",
     "save_dir": f"outputs/{ args.exp_name}/checkpoints",
-    "resume": None if args.resume else None, # Modify as needed
+    "model_name": args.exp_name,
+    "alpha": args.alpha,  # Weight for the divergence loss
+    "divloss": USE_DIVLOSS,  # Use divergence loss
+    "columns_of_interest": args.columns_of_interest,  # Columns of interest for divergence loss
 }
-
 preprocessing_config = {
     "apply_bias_correction": False,
     "apply_denoising": False,
     "apply_intensity_clipping": True,
     "apply_intensity_normalization": True,
     "resample_spacing": (1.0, 1.0, 1.0),
-    "target_shape": (320, 320, 128)  
+    "target_shape": (320, 320, 128),# (256, 256, 96)  (320, 320, 128) # (H, W, D) (272, 272, 128)
 }
+
 config.update(preprocessing_config)
 # WANDB INIT
 wandb_run = wandb.init(project="mama-mia", name=args.exp_name, config=config, job_type="train")
@@ -83,11 +94,9 @@ if USE_MONAI_PIPELINE:
         phase_slice=3,  # 3: pre + 2 phases
     )
 
-    #train_ds = CacheDataset(data=train_files, transform=train_transform, cache_rate=1.0, num_workers=8)
     train_ds = PersistentDataset(data=train_files, transform=train_transform, cache_dir="./persistent_cache_meta/train")
     train_loader = DataLoader(train_ds, batch_size=args.batch_size, shuffle=True, num_workers=4)
-    
-    #val_ds = CacheDataset(data=val_files, transform=val_transform, cache_rate=1.0, num_workers=4)
+
     val_ds = PersistentDataset(data=val_files, transform=val_transform, cache_dir="./persistent_cache_meta/val")
     val_loader = DataLoader(val_ds, batch_size=1, num_workers=4)
 
@@ -115,6 +124,15 @@ if args.model == "unet":
         strides=(2, 2, 2, 2),
         num_res_units=2,
     ).to(device)
+elif args.model == "swinunetr":
+    model = SwinUNETR(img_size=config['target_shape'],
+                        in_channels=3,
+                      out_channels=2,
+                      feature_size=48,
+                      use_checkpoint=True,
+                      )
+    model = model.to("cuda:0")
+    model = torch.nn.DataParallel(model, device_ids=[0, 1])  # Use multiple GPUs if available
 elif args.model == "segresnet":
     model = SegResNet(
         blocks_down=[1, 2, 2, 4],     
@@ -124,18 +142,28 @@ elif args.model == "segresnet":
         out_channels=2,              
         dropout_prob=0.1             
     ).to(device)
-elif args.model == "swinunetr":
-    model = SwinUNETR(
-        img_size=config["target_shape"],
-        in_channels=3,
-        out_channels=2,
-        feature_size=48,
-        use_checkpoint=True,
-    ).to(device)
 else:
     raise ValueError(f"Unknown model type: {args.model}")
 
-loss_function = DiceCELoss(to_onehot_y=True, softmax=True)  
+
+if args.load_checkpoint:
+    # Load the model from the specified checkpoint
+    print(f"Loading model from checkpoint: {CHECKPOINT_PATH}")
+    checkpoint = torch.load(CHECKPOINT_PATH, map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    config['starting_epoch'] = checkpoint['epoch']  # Resume from the last epoch
+    print(f"Resuming training from epoch {checkpoint['epoch']}")
+    wandb.restore(CHECKPOINT_PATH)
+
+if USE_DIVLOSS:
+    # Use DivergenceFairLoss
+    print("Using DivergenceFairLoss")
+    loss_function = DivergenceFairLoss(min_support=args.min_support, device=device, alpha=config['alpha'], sensitive_columns=args.columns_of_interest)
+else:
+    # Use standard DiceCELoss
+    print("Using standard DiceCELoss")
+    loss_function = DiceCELoss(to_onehot_y=True,softmax=True)
+
 
 # TRAINER
 trainer = Trainer(
--- training/trainer.py
+++ training/trainer.py
@@ -50,9 +50,34 @@ class Trainer:
         if training:
             self.optimizer.zero_grad()
             outputs = self.model(images)
-            loss = self.loss_function(outputs, labels)
+            if self.use_divloss:
+                metadata = {}
+                for col in self.config['columns_of_interest']:
+                    metadata[col] = batch[col]
+                new_df = pd.DataFrame(metadata)  
+                try:
+                    self.train_metadata = pd.concat([self.train_metadata, new_df], ignore_index=True)
+                except NameError:
+                    self.train_metadata = new_df
+
+                l_seg = self.dice_loss(outputs, labels)
+                current_weights = self.train_metadata_weights[(batch_idx)* len(labels):(batch_idx+1)*len(labels)].reset_index(drop=True)
+                div_loss = self.loss_function.calculate_divergence_loss(weights=current_weights, 
+                                                                    labels=labels, 
+                                                                    outputs=outputs) 
+            
+                loss = self.config['alpha'] * l_seg + (1 - self.config['alpha']) * div_loss
+            else:
+                loss = self.dice_loss(outputs, labels)            
+            
             loss.backward()
             self.optimizer.step()
+            if self.wandb_run:
+                self.wandb_run.log({"train_loss_batch": loss.item()})
+                if self.use_divloss:
+                    self.wandb_run.log({
+                        "div_loss": div_loss.item(),
+                    })
             return loss.item()
         else:
             with torch.no_grad():
@@ -65,45 +90,145 @@ class Trainer:
     def train_epoch(self, epoch):
         self.model.train()
         running_loss = 0.0
+        if epoch == 1 and self.use_divloss:
+            self.train_metadata_weights = pd.DataFrame(len(self.train_loader.dataset) * [1.0], columns=['weights'])  # Initialize weights to 1.0 for the first epoch
+
         for batch_idx, batch in enumerate(tqdm(self.train_loader, desc=f"Training Epoch {epoch}", total=len(self.train_loader), position=0, leave=True)):
-            # Remove any non-numerical or problematic metadata keys before collate
-            if isinstance(batch, dict):
-                batch = {k: v for k, v in batch.items() if isinstance(v, torch.Tensor)}
-            
-            loss = self._step(batch, training=True)
+            loss = self._step(batch, batch_idx, training=True)
             running_loss += loss
+            if self.wandb_run:
+                self.wandb_run.log({
+                    "epoch": epoch,
+                    "batch": batch_idx
+                })
         return running_loss / len(self.train_loader)
 
-    def validate(self, epoch):
+    def validate(self, epoch=None, compute_scores=False):
+
+        performance_scores, DSC, NormHD = [], [], []
+        val_metadata_with_performance = pd.DataFrame()  # Initialize an empty DataFrame for validation metadata
         self.model.eval()
         self.dice_metric.reset()  # important!
 
         with torch.no_grad():
             for batch_idx, batch in enumerate(tqdm(self.val_loader, desc=f"Validation Epoch {epoch}", total=len(self.val_loader), position=1, leave=True)):
-                if isinstance(batch, dict):
-                    batch = {k: v for k, v in batch.items() if isinstance(v, torch.Tensor)}
-                images = batch["image"].to(self.device).float() #batch["image"][tio.DATA].to(self.device).float()
-                labels = batch["label"].to(self.device).long()  # o .float() si tu loss lo requiere#batch["label"][tio.DATA].to(self.device).long()
+                images = batch["image"].to(self.device).float() 
+                labels = batch["label"].to(self.device).long()  
+
+                val_metadata_dict = {}
+                for col in self.config['columns_of_interest']:
+                    val_metadata_dict[col] = batch[col]
+                val_metadata = pd.DataFrame(val_metadata_dict)
 
                 outputs = self.model(images)
                 probs = torch.nn.functional.softmax(outputs, dim=1)
                 preds = probs.argmax(dim=1, keepdim=True)
 
-                self.dice_metric(preds, labels)
 
+                self.dice_metric(preds, labels)
+                if compute_scores:
+                    performance_score, val_metadata_performance, dsc, normhd = compute_performance_score(
+                        outputs=outputs,
+                        labels=labels,
+                        columns_of_interest=self.config['columns_of_interest'],
+                        val_metadata=val_metadata,
+                    )
+                    performance_scores.append(performance_score)
+                    DSC.append(dsc)
+                    NormHD.append(normhd)
+                    val_metadata_with_performance = pd.concat([val_metadata_with_performance, val_metadata_performance], ignore_index=True)
+                    self.wandb_run.log({
+                        "performance_score": performance_score,
+                    })
+
+                if self.wandb_run:
+                    self.wandb_run.log({
+                        "epoch": epoch,
+                        "batch": batch_idx,
+                        "val_batch_dice": self.dice_metric.aggregate().item(),
+                    })
+
+
+        # Compute the Dice score
         dice_score = self.dice_metric.aggregate().item()
+        # Compute the average scores
+        if len(performance_scores) > 0:
+            avg_performance_score = np.mean(performance_scores)
+            avg_dsc = np.mean(DSC)
+            avg_normhd = np.mean(NormHD)
+        # Compute the fairness and ranking scores
+        if compute_scores:
+            avg_fairness_score, ranking_score = compute_fairness_ranking_score(val_metadata=val_metadata_with_performance,
+                                           columns_of_interest=self.config['columns_of_interest'],
+                                           avg_performance_score=avg_performance_score)
+            if self.wandb_run:
+                self.wandb_run.log({
+                    "epoch": epoch,
+                    "val_performance_score": avg_performance_score,
+                    "val_dice": avg_dsc,
+                    "val_normhd": avg_normhd,
+                    "val_fairness_score": avg_fairness_score,
+                    "val_ranking_score": ranking_score  
+                })
+
+        else:
+            if self.wandb_run:
+                self.wandb_run.log({
+                    "epoch": epoch,
+                    "val_dice": dice_score,                    
+                })    
         return dice_score
+  
+    def _update_weights(self):
+        performance_name = 'performance_score'
+
+        # TEMPORARILY use multi-GPU for scoring
+        # available_devices = [0, 1]  # <--- replace with your desired GPU indices (relative to visible devices)
+        # if torch.cuda.device_count() > 1:
+        #     print(f"Using DataParallel on GPUs: {available_devices}")
+        #     model = torch.nn.DataParallel(self.model, device_ids=available_devices)
+        # else:
+        #     model = self.model
+
+        self.model.eval()
+
+        # Compute performance scores
+        performance_scores, val_metadata = self.loss_function.compute_performance_score(
+            model=self.model,
+            val_loader=self.val_loader
+        )
+
+        # Store scores in metadata
+        val_metadata[performance_name] = performance_scores
+
+        self.train_metadata['weights'] = 1.0
+        weights = self.loss_function.finding_subgroups_weights(
+            val_metadata=val_metadata,
+            train_metadata=self.train_metadata
+        )
+
+        self.train_metadata_weights = weights
+
+        # Clear CUDA memory
+        torch.cuda.empty_cache()
+
 
     def train(self):
-        best_dice = self.best_dice
-        for epoch in range(self.start_epoch, self.config["epochs"] + 1):
-            train_loss = self.train_epoch(epoch)
+        best_dice = 0.0 
+
+        for epoch in range(1 + self.config["starting_epoch"], self.config["epochs"] + 1):
+            train_loss = self.train_epoch(epoch=epoch)
             val_dice = None
-            if epoch % self.val_interval == 0:
-                val_dice = self.validate(epoch)
+            if self.use_divloss:
+                print("Updating weights based on performance scores...")
+                self._update_weights()
+                # Re initiliaze the self.train_metadata
+                self.train_metadata = pd.DataFrame()
+                
+            if epoch % self.config["val_interval"] == 0:
+                val_dice = self.validate(epoch=epoch)
                 print(f"Epoch {epoch}/{self.config['epochs']} - Loss: {train_loss:.4f} - Val Dice: {val_dice:.4f}")
 
-                # Save everything needed to resume training
                 checkpoint = {
                     "epoch": epoch,
                     "model_state_dict": self.model.state_dict(),
@@ -111,9 +236,9 @@ class Trainer:
                     "train_loss": train_loss,
                     "val_dice": val_dice,
                 }
-                model_path = os.path.join(self.save_dir, f"epoch{epoch}.pth")
+                model_path = os.path.join(self.save_dir, f"{self.config['model_name']}_epoch{epoch}.pth")
                 torch.save(checkpoint, model_path)
-                
+
                 if val_dice > best_dice:
                     best_dice = val_dice
                     best_path = os.path.join(self.save_dir, "best_checkpoint.pth")
@@ -124,11 +249,12 @@ class Trainer:
                 print(f"Epoch {epoch}/{self.config['epochs']} - Loss: {train_loss:.4f}")
 
             if self.wandb_run:
-                log_data = {
+                self.wandb_run.log({
                     "epoch": epoch,
                     "train_loss": train_loss,
-                }
+                })
                 if val_dice is not None:
-                    log_data["val_dice"] = val_dice
-                    log_data["best_val_dice"] = best_dice
-                self.wandb_run.log(log_data)
+                    self.wandb_run.log({
+                        "val_dice": val_dice,
+                        "best_val_dice": best_dice,
+                    })
